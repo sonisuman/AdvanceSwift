08e9e303a0d45c648b188446fd940cbafad12431
sonisuman <sonisuman67@gmail.com>
2022-06-18 02:53:30 -0400

    userInputASType example.

--- UserInputAsAtype.playground/Contents.swift
+++ UserInputAsAtype.playground/Contents.swift
@@ -1,3 +1,49 @@
 import UIKit
 
-var greeting = "Hello, playground"
+enum ValidationError: Error {
+  case noEmptyValueAllowed
+  case invalidEmail
+}
+
+func validateEmail(_ email: String) throws {
+  guard !email.isEmpty else {
+    throw ValidationError.noEmptyValueAllowed
+  }
+  let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+  let emailPredicate = NSPredicate(format: "SELF Matches %@",pattern)
+  let isValid  = emailPredicate.evaluate(with: email)
+  if !isValid {
+    throw ValidationError.invalidEmail
+  }
+}
+do{
+  try validateEmail("sonisuman67@gmail.com")
+  print("Email is valid.")
+} catch {
+  print(error)
+}
+
+
+struct Email {
+  let text: String
+  init (_ text: String) throws {
+    guard !text .isEmpty else {
+      throw ValidationError.noEmptyValueAllowed
+    }
+    let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    let emailPredicate = NSPredicate(format: "SELF Matches %@",pattern)
+    let isValid  = emailPredicate.evaluate(with: text)
+    if isValid {
+      self.text = text
+    } else {
+      throw ValidationError.invalidEmail
+    }
+  }
+}
+do{
+  let email =  try  Email.init("sonisuman")
+  print(email)
+} catch {
+  print(error)
+}
+
