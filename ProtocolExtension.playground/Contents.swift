4f1f5152dee8abcde5e8b7f4b33196f96ce4da8e
sonisuman <sonisuman67@gmail.com>
2022-06-21 00:13:58 -0400

    Multiple inheritance in protocol.

--- ProtocolExtension.playground/Contents.swift
+++ ProtocolExtension.playground/Contents.swift
@@ -1,3 +1,59 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol Account {
+  var balance : Double {get set}
+  mutating func deposit(_ amount: Double)
+  mutating func withdraw(_ amount: Double)
+  func transfer(from: Account, to: Account, amount: Double)
+  func calculateInterestEarned() -> Double
+
+}
+struct VerificationRequest {
+    let accounts: [Account]
+}
+
+struct VerificationResponse {
+    let verified: Bool
+}
+
+protocol Verification {
+    func performVerification(_ request: VerificationRequest, completion: (VerificationResponse) -> Void)
+}
+
+extension Verification {
+    func performVerification(_ request: VerificationRequest, completion: (VerificationResponse) -> Void) {
+        // perform the verification
+    }
+}
+extension Account {
+  
+  mutating func deposit(_ amount: Double) {
+    balance += amount
+  }
+  mutating func withdraw(_ amount: Double) {
+    balance -= amount
+  }
+  func calculateInterestEarned() -> Double {
+    return (balance * (0.1/100))
+  }
+}
+
+struct CheckingAccount: Account, Verification {
+  var balance: Double
+  
+  func transfer(from: Account, to: Account, amount: Double) {
+    performVerification(VerificationRequest(accounts: [from, to])) { (response) in
+      if response.verified {
+          // transfer funds
+      }
+    }
+  }
+}
+
+struct MoneyMarketAccounr: Account {
+  var balance: Double
+  
+  func transfer(from: Account, to: Account, amount: Double) {
+    
+  }
+}
